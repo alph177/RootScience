f91df49a02b61771f6c88b409bca1abeb897252f
Alphard <d1025@yahoo.co.jp>
2013-10-15 22:33:24 +0900

    Init

--- mod/org/atg/rootscience/RootScienceCore.java
+++ mod/org/atg/rootscience/RootScienceCore.java
@@ -17,16 +17,6 @@ import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
  
-/**
- * Sample Mod クラス.
- * 
- * Modアノテーションをつけるだけで、Modクラスとして認識される。
- * クラス名は自由に決めることができ、他のクラスを継承する必要もない。
- * なお、Modアノテーションの必須項目は modid のみで、その他は外部ファイルから設定することができ、省略することも可能。
- * 
- * また、NetworkMod は不要であれば省略してもよい。
- * 
- */
 @Mod(modid = "RootScience", version = "Rev.001", name = "RootScience", dependencies="after:Railcraft;after:GregTech-Addon")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class RootScienceCore {
@@ -40,36 +30,16 @@ public class RootScienceCore {
 	public static boolean generateCloth = false , generateKnife = false;
 	public static int clothWeight  = 20, knifeWeight = 8;
 	
-	
-    /**
-     * 指定したIDのModのインスタンス.
-     * 
-     * フィールド名、アクセス指定子はなんでもよい。不要であれば省略可能。
-     * 自身のインスタンスがインジェクションされる。
-     */
-    @Instance("S.U.B.")
+
+    @Instance("RootScience")
     protected static RootScienceCore instance;
- 
-    /**
-     * サーバー、クライアントで異なる処理を行わせる場合に用いるプロキシークラス.
-     * 
-     * フィールド名、はなんでもよい。不要であれば省略可能。(public以外のアクセス指定子が使えるかは不明)
-     * 実行環境にあわせて、どちらかのインスタンスがインジェクションされる。
-     */
+
     
     /*@SideProxy(
         clientSide = "mods.sample.ClientProxy",
         serverSide = "mods.sample.ServerProxy")
     public static SampleProxy proxy;
  */
-    /**
-     * 初期化前処理.
-     * 
-     * メソッド名はなんでもよい。不要であれば省略可能。
-     * 
-     * @param event
-     *         FMLの初期化前処理イベント
-     */
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
 		
@@ -82,15 +52,6 @@ public class RootScienceCore {
     	
     }
    
- 
-    /**
-     * 初期化処理.
-     * 
-     * メソッド名はなんでもよい。不要であれば省略可能。
-     * 
-     * @param event
-     *         FMLの初期化処理イベント
-     */
     @EventHandler
     public void init(FMLInitializationEvent event) {
     	
